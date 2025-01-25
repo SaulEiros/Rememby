206131ce7254c8783b35e7d4492464f01acc025b
sauleiros <sauleiros@gmail.com>
2025-01-26 00:38:50 +0100

    feat: added logging configuration

--- app/handlers/start_handler.py
+++ app/handlers/start_handler.py
@@ -1,7 +1,18 @@
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 
+from app.logger import getLogger
+
+LOG = getLogger(__name__)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    chat = update.effective_chat
+
+    LOG.info(
+       f"User {user.username or user.id} started the application from {chat.type}" 
+    )
+
     await update.message.reply_text("¡Hola! Usa /remind para configurar recordatorios.")
 
 start_handler = CommandHandler("start", start)
